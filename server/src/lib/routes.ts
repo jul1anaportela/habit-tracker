df16c6e347f7702ddf1184b3fbd213968537be1c
jul1anaportela <jul1anaportela@outlook.com>
2023-09-09 17:57:02 -0300

    creating the habit route :sparkles:

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -1,11 +1,39 @@
-import { FastifyInstance } from "fastify"
-import { prisma } from "./prisma"
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import {prisma} from "./prisma"
+import dayjs from 'dayjs'
 
 export async function appRoutes(app: FastifyInstance) {
-    app.get('/hello', async() =>{
-        const habits = await prisma.habit.findMany()
-    
-        return habits
+    app.post('/habits', async(request) => {
+        // title, weekDays
+       
+        const createHabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(
+                z.number().min(0).max(6))
+        })
+
+        // [0,1,2] => Domingo, Segunda, Terça
+
+        const { title, weekDays } = createHabitBody.parse(request.body)
+
+        const today = dayjs().startOf('day').toDate()
+
+        await prisma.habit.create({
+            data:{
+                title,
+                created_at: today,
+                weekDays: {
+                    create: weekDays.map(weekDays => {
+                        return {
+                            week_day: weekDays,
+                        }
+                    })
+                }
+            }
+        })
+
+      
     })
 
 }
